2ab49a858772750cf34d0924328b8b27bcd88958
sencin <asunayuk34@gmail.com>
2026-05-17 16:58:36 +0800

    forgot to add the query

--- src/main/java/com/mintblueberry/KuripotTracker/repository/UserRepository.java
+++ src/main/java/com/mintblueberry/KuripotTracker/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.mintblueberry.KuripotTracker.repository;
 
 import com.mintblueberry.KuripotTracker.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +11,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Boolean existsByEmail(String email);
+
+    @Query(value = "SELECT COUNT(*) FROM users", nativeQuery = true)
+    long keepAlive();
 }
